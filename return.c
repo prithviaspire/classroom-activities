5770e3e3000455d0862fae38185dd74862c8d2fa
prithvix <prithvi.aspire.ku@gmail.com>
2025-12-17 12:17:48 +0545

    Enhance return.c with comments for clarity
    Added comments to explain function definition and call.

--- return.c
+++ return.c
@@ -1,13 +1,15 @@
+
+//Run this code in your local machine
 #include <stdio.h>
-int sum(int a, int b) {
+int sum(int a, int b) {    // Function definition: sum() takes 2 arguments
 int s1 = a + b;
-return s1;
+return s1;                //comment this and run the code see the output
 }
 int main()
 {
 int num1 = 10;
 int num2 = 10;
-int sumof = sum(num1, num2);
+int sumof = sum(num1, num2);        // Function call: sum() with arguments num1, num2
 printf("%d", sumof);
 return 0;
 }
