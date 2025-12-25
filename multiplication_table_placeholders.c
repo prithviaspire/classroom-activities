dc6227883210c66e0acef33b9924dfffb71296de
prithvix <prithvi.aspire.ku@gmail.com>
2025-12-25 15:58:07 +0545

    Add comment for clarity in multiplication table
    Added a comment to clarify the order of operations in the printf statement.

--- multiplication_table_placeholders.c
+++ multiplication_table_placeholders.c
@@ -4,8 +4,9 @@ int main() {
     int i;
 
     for (i = 1; i <= 10; i++) {
-        printf("5 x %d = %d\n", i, 5 * i);
+        printf("5 x %d = %d\n", i, 5 * i);  //misplace this statement as first write 5*i then i
     }
 
     return 0;
 }
+
