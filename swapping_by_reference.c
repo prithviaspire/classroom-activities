2edaaeef3b2173f744d114bf8f0bb656a6c2a0f6
prithvix <prithvi.aspire.ku@gmail.com>
2026-02-05 19:58:20 +0545

    Create swapping_by_reference.c

---
+++ swapping_by_reference.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main(void)
+{
+    int x = 10, y = 20;
+
+    printf("Before swap: x = %d, y = %d\n", x, y);
+
+    swap(&x, &y);   // pass addresses
+
+    printf("After swap: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
