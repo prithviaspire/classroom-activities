de49a3c0c3c126cc71b6ac1f28389576b67a5536
prithvix <prithvi.aspire.ku@gmail.com>
2026-02-26 23:01:43 +0545

    Create matriX_multiplication.c

---
+++ matriX_multiplication.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[3][3], b[3][3], c[3][3];
+    int i, j, k;
+
+    // Input first matrix
+    printf("Enter Values for First Matrix:\n");
+    for(i = 0; i < 3; i++)
+        for(j = 0; j < 3; j++)
+            scanf("%d", &a[i][j]);
+
+    // Input second matrix
+    printf("Enter Values for Second Matrix:\n");
+    for(i = 0; i < 3; i++)
+        for(j = 0; j < 3; j++)
+            scanf("%d", &b[i][j]);
+
+    // Matrix multiplication
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            c[i][j] = 0;
+            for(k = 0; k < 3; k++)
+            {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    // Print multiplication result
+    printf("\nMultiplication of Matrix is:\n");
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 3; j++)
+            printf("%d\t", c[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
